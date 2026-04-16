f501dfddf51b52aa93a8eea2385af6d2842047de
Nilanta Roy Nilay <156948038+Nilanta-Roy-Nilay@users.noreply.github.com>
2026-04-16 22:39:27 +0600

    Update theme_screen.dart

--- Fitness App/new_project/lib/screens/theme_screen.dart
+++ Fitness App/new_project/lib/screens/theme_screen.dart
@@ -1,4 +1,6 @@
 // lib/screens/theme_screen.dart
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../services/theme_service.dart';
